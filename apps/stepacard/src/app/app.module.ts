d6e801848664544a6c85d601ed598173d2aff746
@wendellmva <wmuntslag@live.nl>
2020-08-13 21:30:08 +0300

    feat(admin-page): add home and admin pages

--- apps/stepacard/src/app/app.module.ts
+++ apps/stepacard/src/app/app.module.ts
@@ -2,11 +2,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app.routing';
+import { AdminComponent } from './pages';
+import { HomeComponent } from './pages';
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule],
+  declarations: [
+    AppComponent, 
+    AdminComponent, 
+    HomeComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+  ],
   providers: [],
-  bootstrap: [AppComponent],
+  bootstrap: [
+    AppComponent
+  ],
 })
 export class AppModule {}
